b61309d55df4e7a192aa31831ac697b6c1f47a3c
YalAndrey <74978690+YalAndrey@users.noreply.github.com>
2025-09-22 19:12:07 +0000

    lr3

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,15 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int l = strlen(str);
+	char sv;
+	for (int i = 0; i < l / 2; i++)
+	{
+		sv = str[i];
+		str[i] = str[l- i - 1];
+		str[l- i - 1] = sv;
+	}
 }
 
--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -2,5 +2,7 @@
 
 void Swap(char *left, char *right)
 {
-	// ваш код здесь
+	char center = *left;
+	*left = *right;
+	*right = center;
 }
